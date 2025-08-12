dc7c3ecca310efc7f99a5c8c4e84f3ef82ece4a2
phytorsy01 <phytorsy01@gmail.com>
2025-08-12 22:20:37 +0300

    Initial commit

---
+++ Padre-Ginos/src/routes/__root.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import PizzaOfTheDay from "../PizzaOfTheDay";
+import Header from "../Header";
+import ShoppingCart from "../ShoppingCart"
+import { CartContext } from "../contexts";
+
+export const Route = createRootRoute({
+  component: () => {
+    const cartHook = useState([]);
+    return (
+      <>
+        <CartContext.Provider value={cartHook}>
+          <div>
+            <Header />
+            <Outlet />
+            <PizzaOfTheDay />
+            <ShoppingCart />
+          </div>
+        </CartContext.Provider>
+        <TanStackRouterDevtools />
+        <ReactQueryDevtools />
+      </>
+    );
+  },
+});
